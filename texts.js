2c52587a4935e7f1e203dcbe66c07cdd59323c5f
Rioni <nrosa01@ucm.es>
2021-02-22 12:22:45 +0100

    First commit

---
+++ texts.js
@@ -0,0 +1,9 @@
+const descriptions = {
+    generalDescription: {
+        tittle: "Descripción breve",
+        body: "Esto es una descripción de la tarjeta "+
+              "que espero que en la web se vea bien y tal"
+    },
+}
+
+export default descriptions
